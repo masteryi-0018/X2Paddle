f8c48c2290b27596383138dc0243e04c5ff0db65
co63oc <co63oc@users.noreply.github.com>
2024-11-13 16:34:03 +0800

    Change LOD_TENSOR to DENSE_TENSOR (#1095)

--- x2paddle/project_convertor/pytorch/torch2paddle/nn_init.py
+++ x2paddle/project_convertor/pytorch/torch2paddle/nn_init.py
@@ -118,7 +118,7 @@ class KaimingNormal(MSRAInitializer):
                 ['masra_init', var.name, 'tmp'])),
                                        shape=var.shape,
                                        dtype=out_dtype,
-                                       type=VarDesc.VarType.LOD_TENSOR,
+                                       type=VarDesc.VarType.DENSE_TENSOR,
                                        persistable=False)
         else:
             out_dtype = var.dtype
@@ -189,7 +189,7 @@ class XavierNormal(XavierInitializer):
                 ['xavier_init', var.name, 'tmp'])),
                                        shape=var.shape,
                                        dtype=out_dtype,
-                                       type=VarDesc.VarType.LOD_TENSOR,
+                                       type=VarDesc.VarType.DENSE_TENSOR,
                                        persistable=False)
         else:
             out_dtype = var.dtype
@@ -255,7 +255,7 @@ class XavierUniform(XavierInitializer):
                 ['xavier_init', var.name, 'tmp'])),
                                        shape=var.shape,
                                        dtype=out_dtype,
-                                       type=VarDesc.VarType.LOD_TENSOR,
+                                       type=VarDesc.VarType.DENSE_TENSOR,
                                        persistable=False)
         else:
             out_dtype = var.dtype
